3a7825767760fdc33786a9eaeb9a67cc162364bf
eblondel <emmanuel.blondel1@gmail.com>
2020-04-24 09:17:03 +0200

    pkg init

---
+++ R/atom4R.R
@@ -0,0 +1,27 @@
+#' @name atom4R
+#' @aliases atom4R-package
+#' @aliases atom4R
+#' @docType package
+#'
+#' @import R6
+#' @import methods
+#' @import jsonlite
+#' @import XML
+#' @import plyr
+#'
+#' @title  R package to read/write metadata in Atom XML format.
+#' @description  R engine to orchestrate and run geospatial (meta)data workflows
+#'
+#' @details
+#'  \tabular{ll}{
+#'    Package: \tab atom4R\cr
+#'    Type: \tab Package\cr
+#'    Version: \tab 0.0.9000\cr
+#'    Date: \tab 2020-04-24\cr
+#'    License: \tab MIT\cr
+#'    LazyLoad: \tab yes\cr
+#'  }
+#'
+#'@author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
+#'
+NULL

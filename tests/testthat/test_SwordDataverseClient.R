a3970148a704bbc2c880003b7880ae6b6e3d916e
eblondel <emmanuel.blondel1@gmail.com>
2020-07-27 11:55:17 +0200

    test sword dataverse

---
+++ tests/testthat/test_SwordDataverseClient.R
@@ -0,0 +1,70 @@
+# test_SwordDataverseClient.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for SwordDataverseClient.R
+#=======================
+require(atom4R, quietly = TRUE)
+require(testthat)
+require(XML)
+
+context("SwordDataverseClient")
+
+API <- NULL
+
+test_that("list",{
+  API <- SwordDataverseClient$new(
+    hostname = "<hostname>",
+    token = "<token>",
+    logger = "DEBUG"
+  )
+  expect_is(API, "SwordDataverseClient")
+})
+
+test_that("list Dataverses (Sword collections)",{
+  cols <- API$listCollections() #or getDataverses()
+  expect_is(cols, "list")
+  expect_true(length(cols)>0)
+})
+
+test_that("list Dataverse members",{
+  members <- API$getCollectionMembers("dynids")
+  expect_is(members, "AtomFeed")
+})
+
+test_that("create DC entry",{
+
+  #encoding
+  dcentry <- DCEntry$new()
+  dcentry$setId("my-dc-entry")
+
+  #fill dc entry
+  dcentry$addDCDate(Sys.time())
+  dcentry$addDCTitle("atom4R - Tools to read/write and publish metadata as Atom XML format")
+  dcentry$addDCType("Software")
+  creator <- DCCreator$new(value = "Blondel, Emmanuel")
+  creator$attrs[["affiliation"]] <- "Independent"
+  dcentry$addDCCreator(creator)
+  dcentry$addDCSubject("R")
+  dcentry$addDCSubject("FAIR")
+  dcentry$addDCSubject("Interoperability")
+  dcentry$addDCSubject("Open Science")
+  dcentry$addDCSubject("Dataverse")
+  dcentry$addDCDescription("Atom4R offers tools to read/write and publish metadata as Atom XML syndication format, including Dublin Core entries. Publication can be done using the Sword API which implements AtomPub API specifications")
+  dcentry$addDCPublisher("GitHub")
+
+  funder <- DCContributor$new(value = "CNRS")
+  funder$attrs[["type"]] <- "Funder"
+  dcentry$addDCContributor(funder)
+  dcentry$addDCRelation("Github repository: https://github.com/eblondel/atom4R")
+  dcentry$addDCSource("Atom Syndication format - https://www.ietf.org/rfc/rfc4287")
+  dcentry$addDCSource("AtomPub, The Atom publishing protocol - https://tools.ietf.org/html/rfc5023")
+  dcentry$addDCSource("Sword API - http://swordapp.org/")
+  dcentry$addDCSource("Dublin Core Metadata Initiative - https://www.dublincore.org/")
+  dcentry$addDCSource("Guidelines for implementing Dublin Core in XML - https://www.dublincore.org/specifications/dublin-core/dc-xml-guidelines/")
+  dcentry$addDCLicense("NONE")
+  dcentry$addDCRights("MIT License")
+
+  out <- API$createDataverseRecord("dynids", dcentry)
+
+  expect_is(out, "AtomEntry")
+})

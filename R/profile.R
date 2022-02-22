8fa5de922ed6eec758f5fcb5584420a74561d3f7
mpadge <mark.padgham@email.com>
2022-02-22 14:48:37 +1100

    suppress verbose output from readr

--- R/profile.R
+++ R/profile.R
@@ -11,4 +11,27 @@
   #set DCMI Vocabularies
   setDCMIVocabularies()
 
-} # nocov end
+  old_col_types <- getOption ("readr.show_col_types")
+  if (!is.null (old_col_types)) {
+      options (atom4R.show_col_types = old_col_types)
+  }
+  old_progress <- getOption ("readr.show_progress")
+  if (!is.null (old_progress)) {
+      options (atom4R.show_progress = old_progress)
+  }
+  options (readr.show_col_types = FALSE)
+  options (readr.show_progress = FALSE)
+}
+
+.onUnload <- function (libname, pkgname) {
+
+    if (!is.null (options (atom4R.show_col_types))) {
+        options (readr.show_col_types = options (atom4R.show_col_types))
+    }
+    if (!is.null (options (atom4R.show_progress))) {
+        options (readr.show_progress = options (atom4R.show_progress))
+    }
+    options (atom4R.show_col_types = NULL)
+    options (atom4R.show_progress = NULL)
+}
+# nocov end
